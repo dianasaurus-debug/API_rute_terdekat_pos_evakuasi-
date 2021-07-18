b2b089b327d887c8b925614e688389d9bcdd5d98
Khoiron14 <muh.khoiron14@gmail.com>
2021-07-18 13:15:09 +0700

    Update route

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use App\Http\Controllers\KecamatanController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
 Route::middleware(['auth'])->group(function () {
